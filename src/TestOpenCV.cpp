c1da4b0e54e42e2781a6a2cdb4e2223acdf66e41
Florian van Doorn <Florianvandoorn@hotmail.com>
2026-03-20 11:29:18 +0100

    First commit

---
+++ src/TestOpenCV.cpp
@@ -0,0 +1,70 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+    cout << "OpenCV version: " << CV_VERSION << endl;
+
+    // Load an source image in grayscale.
+    Mat src = imread("RodeBal.jpeg", IMREAD_GRAYSCALE);
+    
+    // pipeline: thresholding
+    Mat dest;
+    threshold(src, dest, 32, 255, THRESH_BINARY);
+
+    // 
+    Mat canny_output;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    Canny(dest, canny_output, 50, 150, 3);
+
+    // find contours
+    findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+    // get the moments
+    vector<Moments> mu(contours.size());
+    for (int i = 0; i < contours.size(); i++)
+    {
+        mu[i] = moments(contours[i], false);
+    }
+
+
+
+    // get the centroid of figures.
+    vector<Point2f> mc(contours.size());
+    for (int i = 0; i < contours.size(); i++)
+    {
+        mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+    }
+
+    // draw contours
+    Mat drawing(canny_output.size(), CV_8UC3, Scalar(255, 255, 255));
+    for (int i = 0; i < contours.size(); i++)
+    {
+        Scalar color = Scalar(167, 151, 0); // B G R values
+        drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+        circle(drawing, mc[i], 10, Scalar(255, 0, 0), -1, 8, 0);
+    }
+
+    if (src.empty()) {
+        cout << "Could not open or find the image. Please add a sample.jpg to the project root." << endl;
+        return -1;
+    }
+
+    // Display the image
+    imshow("Source", src);
+    imshow("Destination", dest);
+    imshow("Canny", canny_output);
+    imshow("Contours", drawing);
+
+    waitKey(0);
+
+    return 0;
+}
