5719d3775e0a017d6873e0560a31dd6228cbdf4e
Florian van Doorn <Florianvandoorn@hotmail.com>
2026-05-01 17:50:24 +0200

    Werkende verbinding tussen esp32 en Debian applicatie. De visionapplicatie simuleert een variërende positie van de bal en stuurt deze via seriële communicatie naar de ESP32. De seriële communicatie is ingesteld op een baudrate van 9600, en de positie wordt verzonden in het formaat "$ActualPosition*\n". De code maakt gebruik van OpenCV voor beeldverwerking en bevat een loop die continu de positie van de bal bijwerkt en verzendt.

--- build/CMakeFiles/main.dir/src/TestOpenCV.cpp.o.d
+++ build/CMakeFiles/main.dir/src/TestOpenCV.cpp.o.d
@@ -373,4 +373,27 @@ CMakeFiles/main.dir/src/TestOpenCV.cpp.o: \
  /usr/include/opencv4/opencv2/video/background_segm.hpp \
  /usr/include/opencv4/opencv2/core/core.hpp \
  /usr/include/opencv4/opencv2/highgui/highgui.hpp \
- /usr/include/opencv4/opencv2/features2d/features2d.hpp
+ /usr/include/opencv4/opencv2/features2d/features2d.hpp \
+ /usr/include/fcntl.h /usr/include/x86_64-linux-gnu/bits/fcntl.h \
+ /usr/include/x86_64-linux-gnu/bits/fcntl-linux.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_iovec.h \
+ /usr/include/linux/falloc.h /usr/include/x86_64-linux-gnu/bits/stat.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_stat.h /usr/include/unistd.h \
+ /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h \
+ /usr/include/x86_64-linux-gnu/bits/getopt_posix.h \
+ /usr/include/x86_64-linux-gnu/bits/getopt_core.h \
+ /usr/include/x86_64-linux-gnu/bits/unistd_ext.h \
+ /usr/include/linux/close_range.h /usr/include/termios.h \
+ /usr/include/x86_64-linux-gnu/bits/termios.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-struct.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-c_cc.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-c_iflag.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-c_oflag.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-baud.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-c_cflag.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-c_lflag.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-tcflow.h \
+ /usr/include/x86_64-linux-gnu/bits/termios-misc.h \
+ /usr/include/x86_64-linux-gnu/sys/ttydefaults.h
--- src/TestOpenCV.cpp
+++ src/TestOpenCV.cpp
@@ -4,6 +4,9 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
 
 using namespace std;
 using namespace cv;
@@ -22,7 +25,31 @@ int main() {
         return -1;
     }
 
-     Mat src, hsv, mask1, mask2, mask3, mask;
+    Mat src, hsv, mask1, mask2, mask3, mask;
+
+    // Open de seriële poort (vervang "/dev/ttyUSB0" door de juiste poortnaam)
+    int serial_port = open("/dev/ttyUSB0", O_RDWR);
+
+    // Check of de seriële poort succesvol is geopend
+    if (serial_port < 0) {
+        perror("Error opening serial port");
+        return -1;
+    }
+
+    termios tty;
+    tcgetattr(serial_port, &tty);
+
+    cfsetispeed(&tty, B115200);
+    cfsetospeed(&tty, B115200);
+
+    tty.c_cflag |= (CLOCAL | CREAD);
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+
+    tcsetattr(serial_port, TCSANOW, &tty);
 
     while (true) {
         
@@ -33,11 +60,15 @@ int main() {
         // Variabele voor de afstand tussen de bal en de border
         double ActualPosition = 0; // Initialize the actual position
 
-        ActualPosition = 10 + (10 * sin(1 * (double) getTickCount() / getTickFrequency())); // Simuleer een variërende positie (vervang dit met echte berekening)
+        ActualPosition = 10 + (10 * sin(10 * (double) getTickCount() / getTickFrequency())); // Simuleer een variërende positie (vervang dit met echte berekening)
 
         // Seriele communicatie met Arduino (vervang "COM3" door de juiste poortnaam)
         // SerialPort serial("COM3", 9600); // Baudrate moet overeenkomen
-        cout << "$" << ActualPosition << "*" << endl;
+
+        string msg = "$" + to_string(ActualPosition) + "*\n";
+        write(serial_port, msg.c_str(), msg.length());  
+
+        //cout << "$" << ActualPosition << "*" << endl;
 
         // Display the image
         imshow("Source", src);
